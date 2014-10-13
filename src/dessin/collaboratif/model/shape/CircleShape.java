bd0a85e93df5c819cb6045776b287c9ffb953016
atoukditlenain <gaborieau.romain@gmail.com>
2014-10-13 19:03:52 +0200

    Ajout des classes modélisant les formes (cercle, rectangle, ligne)

---
+++ src/dessin/collaboratif/model/shape/CircleShape.java
@@ -0,0 +1,45 @@
+package dessin.collaboratif.model.shape;
+
+import dessin.collaboratif.misc.DrawModelEnum;
+
+public class CircleShape extends Shape {
+
+	protected int rayon;
+
+	public int getRayon() {
+		return rayon;
+	}
+
+	public void setRayon(int rayon) {
+		this.rayon = rayon;
+	}
+
+	public CircleShape() {
+		super();
+		super.setType(DrawModelEnum.CIRCLE);
+		setRayon(10);
+	}
+	
+	@Override
+	public String toSVGEntity(String misc) {
+		String baliseName;
+		String entity;
+
+		baliseName = super.getBaliseName();
+
+		entity = "<";
+		entity += baliseName;
+		entity += " cx=\"";
+		entity += getPosition().getWidth();
+		entity += "\" cy=\"";
+		entity += getPosition().getHeight();
+		entity += "\" r=\"";
+		entity += getRayon();
+		entity += "\" ";
+		entity += misc;
+		entity += " />";
+
+		return entity;
+	}
+
+}
