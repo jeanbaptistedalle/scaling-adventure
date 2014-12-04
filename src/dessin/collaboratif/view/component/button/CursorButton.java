a521c0624357cb2346f205b19a5cb69db2f2a4be
Romain GABORIEAU <romain.gaborieau@etud.univ-angers.fr>
2014-12-04 11:18:24 +0100

    Ajout des icones pour les boutons

--- src/dessin/collaboratif/view/component/button/CursorButton.java
+++ src/dessin/collaboratif/view/component/button/CursorButton.java
@@ -1,8 +1,10 @@
 package dessin.collaboratif.view.component.button;
 
+import javax.swing.ImageIcon;
 import javax.swing.JToggleButton;
 
 import dessin.collaboratif.controller.component.button.CursorButtonListener;
+import dessin.collaboratif.misc.GeneralVariables;
 
 public class CursorButton extends JToggleButton {
 
@@ -12,7 +14,8 @@ public class CursorButton extends JToggleButton {
 	private static final long serialVersionUID = -5254659661703089570L;
 
 	public CursorButton() {
-		super("cursor");
+		super("");
+		setIcon(new ImageIcon(GeneralVariables.CURSOR_ICON_PATH));
 		setToolTipText("Selection d'élément");
 		setFocusable(false);
 		this.addActionListener(new CursorButtonListener());
